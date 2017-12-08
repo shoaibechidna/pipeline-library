c2dfc7c0d9a7db897117b3efd5eb8a9e8fb09692
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-08 20:05:57 +0530

    Update mavenjob.groovy

--- vars/mavenjob.groovy
+++ vars/mavenjob.groovy
@@ -1,12 +1,14 @@
 #!/usr/bin/env groovy
 
 def call(String test = 'false') {
-  echo "test=${test}"
+  echo "received test=${test}"
   if (test.equalsIgnoreCase('false')) {
+    echo "running for test false"
     mavenJob('mavenjob') {
       goals('clean install -DskipTests')
     }
   } else{
+    echo "running for test true"
     mavenJob('mavenjob') {
       goals('clean install')
     }
