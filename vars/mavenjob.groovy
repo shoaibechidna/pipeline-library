832e6bffd43b9a9f71f5c6f7d7ac8b326bfa6c56
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-08 21:23:40 +0530

    Update mavenjob.groovy

--- vars/mavenjob.groovy
+++ vars/mavenjob.groovy
@@ -4,9 +4,6 @@ def call(String test = 'false') {
   echo "received test=${test}"
   if (test.equalsIgnoreCase('false')) {
     echo "running for test false"
-    steps {
-      shell('cd spring-petclinic')
-    }
     mavenJob('mavenjob') {
       goals('clean install -DskipTests')
     }
