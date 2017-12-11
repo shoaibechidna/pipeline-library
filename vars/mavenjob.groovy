1d1986da65a685a9cc7b6d5f3b02cb21e4edefd3
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-11 17:25:56 +0530

    Update mavenjob.groovy

--- vars/mavenjob.groovy
+++ vars/mavenjob.groovy
@@ -1,11 +1,11 @@
 
 
-    stages {
-      stage ('install') {
+    
+      
         steps {
           withMaven(maven : 'maven_3_5_0') {
           sh 'mvn clean install -Dskiptest'
           }
         }   
-      }  
-    }
+
+
