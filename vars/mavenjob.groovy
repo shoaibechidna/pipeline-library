8883b577c9eaf6b006d363a42b122c41ec3aa26c
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-11 16:48:43 +0530

    Update mavenjob.groovy

--- vars/mavenjob.groovy
+++ vars/mavenjob.groovy
@@ -3,7 +3,7 @@
 def call(String test = 'false') {
   echo "received test=${test}"
   @Library('utils') 
-  def utils = new Utilities(this)
+
   if (test.equalsIgnoreCase('false')) {
     echo "running for test false"
     node {
