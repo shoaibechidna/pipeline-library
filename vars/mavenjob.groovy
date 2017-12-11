9340355980bd9e00baba3c8560648878785e5e73
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-11 22:03:40 +0530

    Update mavenjob.groovy

--- vars/mavenjob.groovy
+++ vars/mavenjob.groovy
@@ -1,12 +1,7 @@
 
 
 def call(test = 'false') {
-  pipeline {
-                stages {
-                    stage('install123') {
-                        sh 'mvn clean install -DskipTests=true'
-                    }
-                }
-            }   
+  git 'https://github.com/shoaibechidna/pipeline-library.git'
+ 
 }
 
