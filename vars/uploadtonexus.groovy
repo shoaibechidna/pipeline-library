21ceea7c1f45a7cddfdbd4d887aabd94e2bbb4da
shoaibechidna <30625150+shoaibechidna@users.noreply.github.com>
2017-12-28 16:04:35 +0530

    Update uploadtonexus.groovy

--- vars/uploadtonexus.groovy
+++ vars/uploadtonexus.groovy
@@ -1,6 +1,4 @@
 def call(test = 'false') {
-  sh('cd target \n
-     curl -v -u admin:admin123 --upload-file *.jar http://35.224.19.212/repository/mvnjars/japp.jar \n
-     cd ..')
+  sh('cd target && curl -v -u admin:admin123 --upload-file *.jar http://35.224.19.212/repository/mvnjars/japp.jar && cd ..')
 
 }
